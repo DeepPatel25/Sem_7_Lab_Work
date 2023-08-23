a02204ee7c1c328e36cbf601684a3681dab5cfa9
Deep Patel <dp3676991@gmail.com>
2023-08-23 20:47:12 +0530

    Made Changes in Practical 1 of Compiler Design and add user Input functionality

---
+++ Compiler Design/Practical 1/Practical_1_User_Input.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void checkDFA(string inputString, string currState, set<string> &acceptState, map<string, int> &transitions)
+{
+    for (int i = 0; i < inputString.length(); i++)
+    {
+        string temp = currState + inputString[i];
+        currState = to_string(transitions[temp]);
+    }
+
+    if (acceptState.find(currState) != acceptState.end())
+    {
+        cout << inputString << " is valid" << endl;
+    }
+    else
+    {
+        cout << inputString << " is Invalid" << endl;
+    }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int numberOfState, numberOfAlphabet, numberOfAcceptState, numberOfTransitions;
+    string startState;
+    vector<string> state, alphabet;
+    set<string> acceptState;
+    map<string, int> transitions;
+
+    cin >> numberOfState;
+    for (int i = 0; i < numberOfState; i++)
+    {
+        string temp;
+        cin >> temp;
+        state.push_back(temp);
+    }
+
+    cin >> numberOfAlphabet;
+    for (int i = 0; i < numberOfAlphabet; i++)
+    {
+        string temp;
+        cin >> temp;
+        alphabet.push_back(temp);
+    }
+
+    cin >> startState;
+
+    cin >> numberOfAcceptState;
+    for (int i = 0; i < numberOfAcceptState; i++)
+    {
+        string temp;
+        cin >> temp;
+        acceptState.insert(temp);
+    }
+
+    numberOfTransitions = numberOfState * numberOfAlphabet;
+    for (int i = 0; i < numberOfTransitions; i++)
+    {
+        string temp1;
+        int temp2;
+        cin >> temp1 >> temp2;
+
+        transitions[temp1] = temp2;
+    }
+
+    vector<string> userInput = {"aaab", "aaaab", "aa", "b"};
+    for (int i = 0; i < userInput.size(); i++)
+    {
+        checkDFA(userInput[i], startState, acceptState, transitions);
+    }
+
+    return 0;
+}
